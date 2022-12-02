1d605fb310ee3273e7756f7dbe0e0cd35cacbc9a
Liudmila Skripkina <msk.revizor@gmail.com>
2022-12-02 15:30:37 +0300

    add header-menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+"use strict"
+
+/*  меню бургер  */
+const iconMenu = document.querySelector('.icon-menu');
+const menuBody = document.querySelector('.header__menu');
+
+if (iconMenu) {
+   iconMenu.addEventListener("click", function (e) {
+      document.body.classList.toggle('lock');
+      iconMenu.classList.toggle('active');
+      menuBody.classList.toggle('active');
+   });
+}
+// скролл тз меню, кнопок до якоря на странице 
+const anchors = document.querySelectorAll('[data-goto]');
+const timeout = 800;
+
+if (anchors.length > 0) {
+   anchors.forEach(anchor => {
+      anchor.addEventListener("click", onAnchorClick);
+   });
+
+   function onAnchorClick(e) {
+      const anchor = e.target;
+      // проверяем заполнен ли атрибут и существует ли данный объект
+      if (anchor.dataset.goto && document.querySelector(anchor.dataset.goto)) {
+         const gotoBlock = document.querySelector(anchor.dataset.goto);
+         // учитываем высоту шапки
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - document.querySelector('.header__container').offsetHeight + 10;
+         console.log(gotoBlock);
+         console.log(gotoBlockValue);
+
+         if (iconMenu.classList.contains('active')) {
+            document.body.classList.remove('lock');
+            iconMenu.classList.remove('active');
+            menuBody.classList.remove('active');
+         }
+
+         window.scrollTo({
+            top: gotoBlockValue,
+            behavior: 'smooth'
+         });
+         e.preventDefault();
+      }
+   }
+}
+
 /*  поодключение карты  */
 let center = [55.72321706901226, 37.703181999999984];
 
