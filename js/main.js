c7a0c5a2df54150597e2df309558ca7d7f88d7a7
Liudmila Skripkina <msk.revizor@gmail.com>
2022-12-09 18:39:03 +0300

    add posuccessfully-popup

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ if (iconMenu) {
       menuBody.classList.toggle('active');
    });
 }
+
 // скролл тз меню, кнопок до якоря на странице 
 const anchors = document.querySelectorAll('[data-goto]');
 const timeout = 800;
@@ -45,7 +46,7 @@ if (anchors.length > 0) {
    }
 }
 
-/*  поодключение карты  */
+/*  подключение карты  */
 let center = [55.72321706901226, 37.703181999999984];
 
 function init() {
@@ -74,3 +75,130 @@ function init() {
 }
 
 ymaps.ready(init);
+
+// попапы
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+console.log(popupLinks);
+
+let unlock = true;
+
+if (popupLinks.length > 0) {
+   for (let i = 0; i < popupLinks.length; i++) {
+      const popupLink = popupLinks[i];
+      popupLink.addEventListener("click", function (e) {
+         const popupName = popupLink.getAttribute('href').replace('#', '');
+         const curentPopup = document.getElementById(popupName);
+         popupOpen(curentPopup);
+         e.preventDefault();
+      });
+   }
+}
+
+const popupCloseLink = document.querySelectorAll('.close-popup');
+if (popupCloseLink.length > 0) {
+   for (let i = 0; i < popupCloseLink.length; i++) {
+      const el = popupCloseLink[i];
+      el.addEventListener("click", function (e) {
+         popupClose(el.closest('.popup'));
+         e.preventDefault();
+      });
+   }
+}
+
+function popupOpen(curentPopup) {
+   if (curentPopup && unlock) {
+      const popupActive = document.querySelector('.popup.open');
+      if (popupActive) {
+         popupClose(popupActive, false);
+      } else {
+         bodyLock();
+      }
+      curentPopup.classList.add('open');
+      curentPopup.addEventListener("click", function (e) {
+         if (!e.target.closest('.popup__content')) {
+            popupClose(e.target.closest('.popup'));
+         }
+      });
+   }
+}
+
+// при попапах внутри других попапов
+function popupClose(popupActive, doUnlock = true) {
+   if (unlock) {
+      popupActive.classList.remove('open');
+      if (doUnlock) {
+         bodyUnlock();
+      }
+   }
+}
+
+// скрываем скролл, добавляя паддинг body и объектам с position: fixed
+function bodyLock() {
+   const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+   if (lockPadding.length > 0) {
+      for (let i = 0; i < lockPadding.length; i++) {
+         const el = lockPadding[i];
+         el.getElementsByClassName.paddingRight = lockPaddingValue;
+      }
+   }
+   body.style.paddingRight = lockPaddingValue;
+   body.classList.add('lock');
+
+   unlock = false;
+   setTimeout(function () {
+      unlock = true;
+   }, timeout);
+}
+
+// открываем скролл и убираем паддинг у body и объектов с position: fixed
+function bodyUnlock() {
+   setTimeout(function () {
+      if (lockPadding.length > 0) {
+         for (let i = 0; i < lockPadding.length; i++) {
+            const el = lockPadding[i];
+            el.style.paddingRight = '0px';
+         }
+      }
+      body.style.paddingRight = '0px';
+      body.classList.remove('lock');
+   }, timeout);
+
+   unlock = false;
+   setTimeout(function () {
+      unlock = true;
+   }, timeout);
+}
+
+// закрываем попап клавишей Esc
+document.addEventListener("keydown", function (e) {
+   if (e.keyCode == 27) {
+      const popupActive = document.querySelector('.popup.open');
+      if (popupActive)
+         popupClose(popupActive);
+   }
+});
+
+(function () {
+   // проверяем поддержку 
+   if (!Element.prototype.closest) {
+      Element.prototype.closest = function (css) {
+         var node = this;
+         while (node) {
+            if (node.matches(css)) return node;
+            else node = node.parentElement;
+         }
+         return null;
+      };
+   }
+})();
+(function () {
+   // проверяем поддержку 
+   if (!Element.prototype.matches) {
+      Element.prototype.matches = Element.prototype.matchesSelector ||
+         Element.prototype.mozmatchesSelector ||
+         Element.prototype.matchesSelector;
+   }
+})();
